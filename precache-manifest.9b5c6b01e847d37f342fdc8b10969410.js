35033be7b2cff12e8a215aa80415f3b1f93c29c8
vishnu0320 <vishnu.gupta@daffodilsw.com>
2020-08-23 14:47:43 +0530

    Updates

---
+++ precache-manifest.9b5c6b01e847d37f342fdc8b10969410.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1aa2d0bd16d2a1f5fe34ae28de29ccd",
+    "url": "/qbank/index.html"
+  },
+  {
+    "revision": "811e35b495bd393b23f9",
+    "url": "/qbank/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c45f570ff82f76d1a612",
+    "url": "/qbank/static/css/main.c3a25330.chunk.css"
+  },
+  {
+    "revision": "811e35b495bd393b23f9",
+    "url": "/qbank/static/js/2.c65af7a4.chunk.js"
+  },
+  {
+    "revision": "14b248e42580150a92d48bdaea7f7b3a",
+    "url": "/qbank/static/js/2.c65af7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45f570ff82f76d1a612",
+    "url": "/qbank/static/js/main.50f76574.chunk.js"
+  },
+  {
+    "revision": "e3dc90d5612216c60b56",
+    "url": "/qbank/static/js/runtime-main.09c03c10.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/qbank/static/media/logo.ee7cd8ed.svg"
+  }
+]);
